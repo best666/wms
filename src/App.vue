6247fe93eafffe94e84c87bb3a1b4174c24f675a
best <754393889@qq.com>
2021-05-12 18:53:11 +0800

    代码构建

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,6 @@
     <div>
       <main-tab-bar></main-tab-bar>
     </div>
-<!--    <div>-->
-<!--      <router-link to="/home">首页</router-link>-->
-<!--      <router-link to="/stores">仓库</router-link>-->
-<!--      <router-link to="/about">我的</router-link>-->
-<!--    </div>-->
-
 <!--  路由匹配到的组件将显示在这里-->
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-//import Out from "@/views/home/childComps/Out";
-//import Transfer from "@/views/home/childComps/Transfer";
-//import Home from '../views/Home.vue'
-//import view from "vue-router/src/components/view";
 
 //路由懒加载
 const Home = () => import('../views/home/Home');
-const In = () => import('@/views/home/childComps/In');
-const Out = () => import('@/views/home/childComps/Out');
-const Transfer = () => import("@/views/home/childComps/Transfer")
-
 const Stores = () => import('../views/warehouse/Stores');
 const About = () => import('../views/profile/About');
 
@@ -27,20 +19,6 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home,
-    // redirect: '/in',
-    children:[{
-      path:'/in',
-      name:'In',
-      component: In
-    },{
-      path: '/out',
-      name: 'Out',
-      component:Out
-    },{
-      path: '/transfer',
-      name: 'Transfer',
-      component : Transfer
-    }]
   },
   {
     path: '/about',
